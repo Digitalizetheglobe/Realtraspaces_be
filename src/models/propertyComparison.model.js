3db937193e52abace939014b073e8304ed1c8c46
Prasadasw <prasadaswar6870@gmail.com>
2025-07-12 12:28:50 +0530

    new changes

--- src/models/propertyComparison.model.js
+++ src/models/propertyComparison.model.js
@@ -6,11 +6,13 @@ const PropertyComparison = sequelize.define('PropertyComparison', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        field: 'id'
     },
     webUserId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'webUserId', // Explicitly set to match database column name
         references: {
             model: WebUser,
             key: 'id'
@@ -20,20 +22,34 @@ const PropertyComparison = sequelize.define('PropertyComparison', {
     },
     propertyId: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        field: 'propertyId' // Explicitly set to match database column name
     },
     propertyData: {
         type: DataTypes.JSON,
-        allowNull: false
+        allowNull: false,
+        field: 'propertyData' // Explicitly set to match database column name
     },
     addedAt: {
         type: DataTypes.DATE,
         allowNull: false,
-        defaultValue: DataTypes.NOW
+        defaultValue: DataTypes.NOW,
+        field: 'addedAt' // Explicitly set to match database column name
     }
 }, {
     tableName: 'property_comparisons',
     timestamps: false,
+    // Disable automatic transformation of field names
+    // to prevent any implicit snake_case conversion
+    underscored: false,
+    // Explicitly define the field mapping
+    fieldMap: {
+        id: 'id',
+        webUserId: 'webUserId',
+        propertyId: 'propertyId',
+        propertyData: 'propertyData',
+        addedAt: 'addedAt'
+    },
     indexes: [
         {
             unique: true,
@@ -43,14 +59,16 @@ const PropertyComparison = sequelize.define('PropertyComparison', {
     ]
 });
 
-// Define association
+// Define association with explicit field names
 WebUser.hasMany(PropertyComparison, { 
     foreignKey: 'webUserId',
-    as: 'propertyComparisons' 
+    targetKey: 'id',
+    as: 'propertyComparisons'
 });
 
 PropertyComparison.belongsTo(WebUser, { 
     foreignKey: 'webUserId',
+    targetKey: 'id',
     as: 'user'
 });
 
--- src/models/savedProperty.model.js
+++ src/models/savedProperty.model.js
@@ -6,11 +6,13 @@ const SavedProperty = sequelize.define('SavedProperty', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        field: 'id'
     },
     webUserId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'webUserId', // Explicitly set to match database column name
         references: {
             model: WebUser,
             key: 'id'
@@ -20,19 +22,33 @@ const SavedProperty = sequelize.define('SavedProperty', {
     },
     propertyId: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        field: 'propertyId' // Explicitly set to match database column name
     },
     propertyData: {
         type: DataTypes.JSON,
-        allowNull: false
+        allowNull: false,
+        field: 'propertyData' // Explicitly set to match database column name
     },
     savedAt: {
         type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
+        defaultValue: DataTypes.NOW,
+        field: 'savedAt' // Explicitly set to match database column name
     }
 }, {
     tableName: 'saved_properties',
     timestamps: false,
+    // Disable automatic transformation of field names
+    // to prevent any implicit snake_case conversion
+    underscored: false,
+    // Explicitly define the field mapping
+    fieldMap: {
+        id: 'id',
+        webUserId: 'webUserId',
+        propertyId: 'propertyId',
+        propertyData: 'propertyData',
+        savedAt: 'savedAt'
+    },
     indexes: [
         {
             unique: true,
@@ -42,14 +58,16 @@ const SavedProperty = sequelize.define('SavedProperty', {
     ]
 });
 
-// Define association
+// Define association with explicit field names
 WebUser.hasMany(SavedProperty, { 
     foreignKey: 'webUserId',
+    targetKey: 'id',
     as: 'savedProperties'
 });
 
 SavedProperty.belongsTo(WebUser, { 
     foreignKey: 'webUserId',
+    targetKey: 'id',
     as: 'user'
 });
 
