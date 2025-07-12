5db9b1d7a519524424218745acc8d9ce39522a0d
Prasadasw <prasadaswar6870@gmail.com>
2025-07-12 12:14:59 +0530

    new

--- src/models/propertyComparison.model.js
+++ src/models/propertyComparison.model.js
@@ -6,11 +6,13 @@ const PropertyComparison = sequelize.define('PropertyComparison', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        field: 'id'
     },
     webUserId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'web_user_id', // Explicitly set the database column name
         references: {
             model: WebUser,
             key: 'id'
@@ -20,31 +22,42 @@ const PropertyComparison = sequelize.define('PropertyComparison', {
     },
     propertyId: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        field: 'property_id' // Explicitly set the database column name
     },
     propertyData: {
         type: DataTypes.JSON,
-        allowNull: false
+        allowNull: false,
+        field: 'property_data' // Explicitly set the database column name
     },
     addedAt: {
         type: DataTypes.DATE,
         allowNull: false,
-        defaultValue: DataTypes.NOW
+        defaultValue: DataTypes.NOW,
+        field: 'added_at' // Explicitly set the database column name
     }
 }, {
     tableName: 'property_comparisons',
     timestamps: false,
+    underscored: true, // This will automatically convert camelCase to snake_case for all fields
     indexes: [
         {
             unique: true,
-            fields: ['webUserId', 'propertyId'],
+            fields: ['web_user_id', 'property_id'], // Use the actual column names
             name: 'unique_webuser_property'
         }
     ]
 });
 
 // Define association
-WebUser.hasMany(PropertyComparison, { foreignKey: 'webUserId' });
-PropertyComparison.belongsTo(WebUser, { foreignKey: 'webUserId' });
+WebUser.hasMany(PropertyComparison, { 
+    foreignKey: 'web_user_id', // Use the actual column name
+    as: 'propertyComparisons' 
+});
+
+PropertyComparison.belongsTo(WebUser, { 
+    foreignKey: 'web_user_id', // Use the actual column name
+    as: 'user'
+});
 
 module.exports = PropertyComparison;
--- src/models/savedProperty.model.js
+++ src/models/savedProperty.model.js
@@ -6,41 +6,57 @@ const SavedProperty = sequelize.define('SavedProperty', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        field: 'id'
     },
     webUserId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'web_user_id', // Explicitly set the database column name
         references: {
             model: WebUser,
             key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
     },
     propertyId: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        field: 'property_id' // Explicitly set the database column name
     },
     propertyData: {
         type: DataTypes.JSON,
-        allowNull: false
+        allowNull: false,
+        field: 'property_data' // Explicitly set the database column name
     },
     savedAt: {
         type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
+        defaultValue: DataTypes.NOW,
+        field: 'saved_at' // Explicitly set the database column name
     }
 }, {
     tableName: 'saved_properties',
     timestamps: false,
+    underscored: true, // This will automatically convert camelCase to snake_case for all fields
     indexes: [
         {
             unique: true,
-            fields: ['webUserId', 'propertyId']
+            fields: ['web_user_id', 'property_id'], // Use the actual column names
+            name: 'unique_webuser_saved_property'
         }
     ]
 });
 
 // Define association
-WebUser.hasMany(SavedProperty, { foreignKey: 'webUserId' });
-SavedProperty.belongsTo(WebUser, { foreignKey: 'webUserId' });
+WebUser.hasMany(SavedProperty, { 
+    foreignKey: 'web_user_id', // Use the actual column name
+    as: 'savedProperties'
+});
+
+SavedProperty.belongsTo(WebUser, { 
+    foreignKey: 'web_user_id', // Use the actual column name
+    as: 'user'
+});
 
 module.exports = SavedProperty;
